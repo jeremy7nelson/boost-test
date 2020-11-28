dac14463176ad2de8b14ce5aed1755f805ebe7a5
Jeremy Nelson <jeremy@biosensinginstrument.com>
2020-11-27 17:11:40 -0700

    Update setup.sh and remove clang-8

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-sudo apt install build-essential git python-numpy -y
+sudo apt install build-essential git python2 -y
 sudo apt install g++-8 g++-9 g++-10 -y
-sudo apt install clang-8 clang-9 clang-10 clang-11 -y
+sudo apt install clang-9 clang-10 clang-11 -y
 sudo apt install libopenmpi-dev -y
+sudo ln /usr/bin/python2 /usr/bin/python
